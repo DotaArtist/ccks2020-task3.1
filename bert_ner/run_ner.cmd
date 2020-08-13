bfe55319af5da9805bf4df7eb26e35a614e121c1
yp <desplode@outlook.com>
2020-08-13 14:25:01 +0800

    ...

---
+++ bert_ner/run_ner.cmd
@@ -0,0 +1,2 @@
+# bert fine-tuning 短句128
+python BERT_NER.py --task_name="NER" --do_lower_case=True --crf=True --do_train=True --do_eval=True --do_predict=True --data_dir=mydata --vocab_file=D:/model_file/chinese_L-12_H-768_A-12/vocab.txt --bert_config_file=D:/model_file/chinese_L-12_H-768_A-12/bert_config.json --init_checkpoint=D:/model_file/chinese_L-12_H-768_A-12/bert_model.ckpt --max_seq_length=128 --train_batch_size=16 --learning_rate=2e-5 --num_train_epochs=4.0 --output_dir=./output/result_dir
